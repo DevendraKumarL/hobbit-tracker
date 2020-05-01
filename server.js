c5ff633f44673cc7f10a3ecb13f7e55d54e19d66
Devendra Kumar L <dkumarl@vmware.com>
2020-05-01 11:24:53 +0530

    Installed npm packages for clarity_ui chart_js libs. Removed all CDNs

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use('/clrui', express.static(__dirname + '/node_modules/@clr/ui'));
+app.use('/clricons', express.static(__dirname + '/node_modules/@clr/icons'));
+app.use('/webcomponents', express.static(__dirname + '/node_modules/@webcomponents/custom-elements'));
+app.use('/chartjs', express.static(__dirname + '/node_modules/chart.js/dist'));
+app.use('/chartjsplugin', express.static(__dirname + '/node_modules/chartjs-plugin-datalabels/dist'));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
